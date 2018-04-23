f378fe330cc8dc5ab1030bc83015f5e8742180eb
Lyulyaev Maxim <lyulyaevma@lad24.ru>
2018-04-23 15:40:59 +0300

    Обновил до Webpack 4. Интегрировал PostCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,178 +1,195 @@
-const path = require('path');
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
-const WriteFilePlugin = require('write-file-webpack-plugin');
-const RobotstxtPlugin = require('robotstxt-webpack-plugin').default;
-const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
+const path = require('path')
+const webpack = require('webpack')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin')
+
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const WriteFilePlugin = require('write-file-webpack-plugin')
+const RobotstxtPlugin = require('robotstxt-webpack-plugin').default
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const CssChunksHtmlWebpackPlugin = require('css-chunks-html-webpack-plugin')
+// const ExtractCssChunks = require('extract-css-chunks-webpack-plugin')
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const pkg = require('./package.json');
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
-const NODE_ENV = process.env.NODE_ENV || 'production';
-const isProduction = (NODE_ENV === 'production');
-const isDevelopment = (NODE_ENV === 'development');
 const root = path.resolve(__dirname, './')
 
-const webpackConfig = {
-  entry: isDevelopment
-    ? [
-      'react-hot-loader/patch', // activate HMR for React
-      `webpack-hot-middleware/client?path=http://${HOST}:${PORT}/__webpack_hmr`, // bundle the client for webpack-hot-middleware and connect to the provided endpoint
+const mode = process.env.NODE_ENV
+const isDev = mode === 'development'
 
-      './src/client.tsx',
-    ]
-    : [
-      './src/client.tsx',
-    ],
+module.exports = /*(env, argv) => {
+	const mode = typeof env !== 'undefined' ? env : argv.mode
+	const isDev = mode === 'development'
+	console.log(`mode: ${mode}, isDev: ${isDev}`)
 
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-  },
-
-  output: {
-    path: path.join(__dirname, 'dist/public/'),
-    chunkFilename: 'assets/scripts/[name].[chunkhash].js',
-    filename: isDevelopment
-      ? 'main.js'
-      : 'assets/scripts/[name].[chunkhash].js',
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: ['css-hot-loader'].concat(
-          ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  minimize: isProduction,
-                  sourceMap: !isProduction
-                },
-              },
-              {
-                loader: 'sass-loader',
-                options: {
-                  sourceMap: !isProduction
-                }
-              },
-            ],
-          })
-        ),
-      },
-      {
-        test: /\.(ts|tsx)?$/,
-        use: [
-          {
-            loader: 'awesome-typescript-loader',
-            options: {
-              configFileName: `${root}/configs/tsconfig.json`,
-              reportFiles: [`${root}/src/**/*.{ts,tsx}`],
-              useCache: true,
-              //usePrecompiledFiles: true, //использовать js файлы
-              //errorsAsWarnings: true, //вместо ошибок TS даёт предупреждения,
-              forceIsolatedModules: true,
-              //useTranspileModule: true, //режим быстрой генерации
-              useBabel: true,
-              babelCore: '@babel/core',
-              babelOptions: require(`${root}/configs/babelrc`)
-            },
-          }
-        ],
-        include: path.join(__dirname, 'src'),
-        exclude: /node_modules/,
-      },
-    ],
-  },
-
-	// optimization.splitChunks
-
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-    }),
-
-    isDevelopment
-      ? null
-      : new webpack.optimize.ModuleConcatenationPlugin(),
-
-    isDevelopment
-      ? new webpack.HotModuleReplacementPlugin() // enable HMR globally
-      : null,
-    isDevelopment
-      ? new webpack.NamedModulesPlugin() // prints more readable module names in the browser console on HMR updates
-      : null,
-    isDevelopment
-      ? new webpack.NoEmitOnErrorsPlugin() // do not emit compiled assets that include errors
-      : null,
-
-    new ExtractTextPlugin({
-      filename: isDevelopment
-        ? 'assets/styles/main.css'
-        : 'assets/styles/[name].[chunkhash].css',
-    }),
+	return */{
+		mode,
+		entry: isDev
+			? [
+				'react-hot-loader/patch',
+				`webpack-hot-middleware/client?path=http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}/__webpack_hmr`,
+				'./src/client.tsx',
+			]
+			: [
+				'./src/client.tsx',
+			],
 
 
-	  isProduction
-      ? new UglifyJSPlugin({
-			  // sourceMap: true,
-			  cache: true,
-			  parallel: true,
-			  uglifyOptions: {
-				  mangle: true
-				  // compress: false
-			  }
-		  })
-      : null,
+		output: {
+			path: path.join(__dirname, 'dist/public/'),
+			chunkFilename: 'assets/scripts/[name].[chunkhash].js',
+			filename: isDev
+				? 'main.js'
+				: 'assets/scripts/[name].[chunkhash].js',
+		},
 
-    isDevelopment
-      ? null
-      : new webpack.BannerPlugin(`${pkg.version} ${new Date().toString()}`),
+		resolve: {
+			extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+		},
 
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
-      minify: isProduction ? { collapseWhitespace: true, collapseInlineTagWhitespace: true } : false,
-      alwaysWriteToDisk: true,
-    }),
-    new HtmlWebpackHarddiskPlugin(),
+		devtool: !isDev
+			? 'none'
+			: 'source-map',
 
-    new CopyWebpackPlugin([
-      {
-        context: 'src/assets',
-        from: '**/*',
-        to: 'assets',
-        ignore: ['styles/**/*']
-      }
-    ]),
+		performance: {
+			maxAssetSize: 500000,
+		},
 
-    new RobotstxtPlugin({
-      policy: [
-        isProduction
-          ? { userAgent: '*', allow: '/' }
-          : { userAgent: '*', disallow: '/' },
-      ],
-    }),
+		// optimization.splitChunks, Chunks.groupsIterable
 
-    new WriteFilePlugin(),
+		plugins: [
+			/*isDev
+				? null
+				: new webpack.optimize.ModuleConcatenationPlugin(),*/
 
-    // isDevelopment ? null : new BundleAnalyzerPlugin()
-  ].filter(Boolean),
+			isDev
+				? new webpack.HotModuleReplacementPlugin() // enable HMR globally
+				: null,
 
-  devtool: isProduction
-    ? 'none'
-    : 'source-map',
+			/*isDev
+				? new webpack.NamedModulesPlugin() // prints more readable module names in the browser console on HMR updates
+				: null,*/
 
-  performance: {
-    maxAssetSize: 500000,
-  },
-};
+			/*isDev
+				? new webpack.NoEmitOnErrorsPlugin() // do not emit compiled assets that include errors
+				: null,*/
 
-module.exports = webpackConfig;
+			new MiniCssExtractPlugin({
+				filename: isDev
+					? 'assets/styles/main.css'
+					: 'assets/styles/[name].[chunkhash].css',
+				// filename: 'css/[name].[contenthash].css',
+				ignoreOrder: true //для css-modules
+			}),
+
+			!isDev
+				? new UglifyJSPlugin({
+					// sourceMap: true,
+					cache: true,
+					parallel: true,
+					uglifyOptions: {
+						mangle: true
+						// compress: false
+					}
+				})
+				: null,
+
+			isDev
+				? null
+				: new webpack.BannerPlugin(`${pkg.version} ${new Date().toString()}`),
+
+			new HtmlWebpackPlugin({
+				template: path.resolve(__dirname, 'src/index.html'),
+				minify: !isDev ? {collapseWhitespace: true, collapseInlineTagWhitespace: true} : false,
+				alwaysWriteToDisk: true,
+			}),
+			new HtmlWebpackHarddiskPlugin(),
+
+			new CopyWebpackPlugin([
+				{
+					context: 'src/assets',
+					from: '**/*',
+					to: 'assets',
+					ignore: ['styles/**/*']
+				}
+			]),
+
+			new RobotstxtPlugin({
+				policy: [
+					!isDev
+						? {userAgent: '*', allow: '/'}
+						: {userAgent: '*', disallow: '/'},
+				],
+			}),
+
+			new WriteFilePlugin(),
+
+			// isDev ? null : new BundleAnalyzerPlugin()
+		].filter(Boolean),
+
+		module: {
+			rules: [
+				{
+					test: /\.(ts|tsx)?$/,
+					use: [
+						{
+							loader: 'awesome-typescript-loader',
+							options: {
+								configFileName: `${root}/configs/tsconfig.json`,
+								reportFiles: [`${root}/src/**/*.{ts,tsx}`],
+								useCache: true,
+								//usePrecompiledFiles: true, //использовать js файлы
+								//errorsAsWarnings: true, //вместо ошибок TS даёт предупреждения,
+								forceIsolatedModules: true,
+								//useTranspileModule: true, //режим быстрой генерации
+								useBabel: true,
+								babelCore: '@babel/core',
+								babelOptions: require(`${root}/configs/babelrc`)
+							},
+						}
+					],
+					include: path.join(__dirname, 'src'),
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader
+						},
+						{
+							loader: 'css-loader',
+							options: {
+								importLoaders: 1,
+								modules: true,
+								localIdentName: '[local]-[hash:base64:4]',
+								sourceMap: isDev ? true : false
+							}
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								ident: 'postcss',
+								config: {
+									path: `${root}/configs/postcss.config.js`
+								},
+								sourceMap: isDev ? 'inline' : false
+							}
+						},
+						{
+							loader: 'stylefmt-loader',
+							options: {
+								//config: `${root}/configs/.stylelintrc`
+							}
+						}
+					]
+				},
+			],
+		}
+	}
+// }
